1e7396247f078d28814d03c07ba869e36ffc9059
Jonathan Kim <157512069+jonathankimmy@users.noreply.github.com>
2024-04-25 15:17:22 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/https://github.com/jonathankimmy/class-25-pwa/sw.js', { scope: '/https://github.com/jonathankimmy/class-25-pwa/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/class-25-pwa/sw.js', { scope: '/class-25-pwa/' })})}
